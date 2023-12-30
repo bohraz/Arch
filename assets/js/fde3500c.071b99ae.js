298b497449088fec9057589e63ecd144aaec3d1a
Jai <jai-cm@outlook.com>
2023-12-30 02:29:56 -0600

    Built and published by Moonwave

---
+++ assets/js/fde3500c.071b99ae.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[869],{3891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/bohraz/Arch/blob/main","baseUrl":"/Arch/","classOrder":[],"apiCategories":[]}')}}]);
